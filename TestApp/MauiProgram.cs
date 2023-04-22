b75f0f6f618018cc6beb1f3a5f074a4a2be3cd97
Nathan Moebus <nathan.moebus@wolterskluwer.com>
2023-04-22 17:49:43 -0400

    update

--- TestApp/MauiProgram.cs
+++ TestApp/MauiProgram.cs
@@ -1,24 +1,19 @@
 ﻿using Microsoft.Extensions.Logging;
 
-namespace TestApp;
 
-public static class MauiProgram
+namespace TestApp
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-            .ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .UseMauiMaps();
 
-#if DEBUG
-		builder.Logging.AddDebug();
-#endif
 
-		return builder.Build();
-	}
+            return builder.Build();
+        }
+    }
 }

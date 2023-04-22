821d71de6597607c0140f298e3d5ee9209f1dbbc
Nathan Moebus <nathan.moebus@wolterskluwer.com>
2023-04-22 17:41:31 -0400

    Add project files.

---
+++ TestApp/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace TestApp;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    public void TestButton_Clicked(object sender, EventArgs e)
+    {
+        TestFrame.IsVisible = true;
+        TestButton.IsVisible = false;
+
+    }
+
+    public void HideFrameButton_Clicked(object sender, EventArgs e)
+    {
+        TestFrame.IsVisible = false;
+        TestButton.IsVisible = true;
+    }
+}
